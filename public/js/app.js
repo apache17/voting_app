ec73803a721ab7346b4db5f570a317aa7c202487
Boris Garcia <brgp@live.com>
2018-03-10 00:42:37 -0600

    Fin

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 class ProductList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
+    state = {
       products: [],
     };
     this.handleProductUpVote = this.handleProductUpVote.bind(this);
@@ -12,7 +12,7 @@ class ProductList extends React.Component {
     this.setState({ products: Seed.products });
   }
 
-  handleProductUpVote(productId) {
+  handleProductUpVote = (productId) => {
     const nextProducts = this.state.products.map((product) => {
       if (product.id === productId) {
         return Object.assign({}, product, {
@@ -58,9 +58,10 @@ class Product extends React.Component {
     this.handleUpVote = this.handleUpVote.bind(this);
   }
 
-  handleUpVote() {
-    this.props.onVote(this.props.id);
-  }
+  handleUpVote = () => (
+    this.props.onVote(this.props.id)
+  );
+
   render() {
     return (
       <div className='item'>
